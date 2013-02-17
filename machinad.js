c82a7ae0f2b2009f3410fec9bc9dbb77972c27d3
Your Name <you@example.com>
2013-02-17 03:00:06 -0700

    Removing the error monad from the generic monadic constructs

--- machinad.js
+++ machinad.js
@@ -29,57 +29,71 @@
 //    in your modified or derived version, because I'm not a dick. You still
 //    must follow 1f above in any derived works or modified forms of this.
 
+var makeMonadWithAlternative = function(m_, bind_, alt_, ret_) {
+    (function() {
+        /* monad instance */
+        var bind = function(f, m) {
+            return m !== undefined && m.m !== undefined && m.a !== undefined ? bind_(f, m) : m;
+        };
+        var ret = ret_;
+        var kleisli = function(f1, f2) { return function(a) { return bind(f2, f1(a)) }; };
+
+        var liftM2 = function(f, m1, m2) {
+            return bind(function (fp) {
+                return bind(function(a1) {
+                    return bind(function(a2) {
+                        return ret(fp(a1)(a2));
+                    }, m2);
+                }, m1);
+            }, ret(f));
+        };
+
+        /* functor instance */
+        var map = function(m, f) { return bind(function(a) { return ret(f(a)); }, m); };
+
+        /* applicative instance; ret = return = pure */
+        var ap = function(mf, ma) {
+            return liftM2(function(a) { return a; }, mf, ma);
+        };
+
+        /* alternative instance */
+        var alt = alt_;
+
+        /* recursive kleisli fold until alternative is chosen */
+        var until = function(f, p) {
+            return function(a) {
+                return kleisli(f, alt(p, until(f, p)))(a); 
+            };
+        };
+
+        (function(a, k, u) {
+            (function() {
+                this.or = function(f2) { return a(this, f2); };
+                this.then = function(f2) { return k(this, f2); };
+                this.until = function(p) { return u(this, p); };
+            }).call(Function.prototype);
+        })(alt, kleisli, until);
+
+        (function(b, r, m, a) {
+            (function() {
+                this.bind = function(f) { return b(f, this); };
+                this.ret = function(a) { return r(a); };
+                this.map = function(f) { return m(this, f); };
+                this.ap = function(mf) { return a(mf, this); };
+            }).call(m_.prototype);
+        })(bind, ret, map, ap);
+
+    }).call(m_.prototype);
+};
+
+// create the data type and monad instance
 (function() {
-    this.m_ = {};
-
-    /* canContinue: error monad gate, and general monad validator */
-    this.m_.canContinue = function(m) { return m !== undefined && m.m !== undefined && m.m === Success && m.a !== undefined; };
-
-    /* functor instance */
-    this.m_.map = function(m, f) { return m.m === Failure ? new m.m.f(m.a) : new m.m.f(f(m.a)); };
-
-    /* applicative instance; ret = return = pure */
-    this.m_.ret = function(a) { return new success(a); };
-    this.m_.ap = function(mf, ma) {
-        return
-             mf.m === Failure ? new mf.m.f(mf.a) :
-            (ma.m === Failure ? new ma.m.f(ma.a) :
-                                new success(mf.a(ma.a)));
+    var dom = function(){};
+    var either = function(m, a) {
+        this.m = m;
+        try { this.a = JSON.parse(JSON.stringify(a)); }
+        catch(e) { this.a = $.extend(true, {}, a); }
     };
-
-    /* alternative instance */
-    this.m_.alternative = function(f1, f2) {
-        return function(a) {
-            var resultf1 = f1(a);
-            if (resultf1.m === Success) return resultf1;
-            return f2(a);
-        };
-    };
-
-    /* monad instance */
-    this.m_.bind = function(f, m) { return m_.canContinue(m) ? f(m.a) : m; };
-    this.m_.kleisli = function(f1, f2) { return function(a) { return m_.bind(f2, f1(a)) }; };
-
-    /* error monad's monadic actions, and some english verbiage aliases */
-    this.m_.until = function(f, p) {
-        return function(a) {
-            return m_.kleisli(f, m_.alternative(p, m_.until(f, p)))(a); 
-        };
-    };
-
-    this.m_.run = function(f, a) { return m_.bind(f, m_.ret(a)); };
-
-    this.m_.or = this.m_.alternative;
-    this.m_.then = this.m_.kleisli;
-
-
-   (function() {
-       this.or = function(f2) { return m_.or(this, f2); };
-       this.then = function(f2) { return m_.then(this, f2); };
-       this.until = function(p) { return m_.until(this, p); };
-   }).call(Function.prototype);
-
-    var either = function(m, a) { this.m = m; this.a = a; };
 
     var Success = {}
     var Failure = {}
@@ -87,4 +101,16 @@
     Success.f = this.success = function(a) { return new either(Success, a); };
     Failure.f = this.failure = function(a) { return new either(Failure, a); };
 
-}).call(this);
+    var bind = function(f, m) { return m.m === Success ? f(m.a) : m; };
+    var ret = function(a) { return new success(a); };
+
+    var alt = function(f1, f2) {
+        return function(a) {
+            var resultf1 = f1(a);
+            return resultf1.m === Success ? resultf1 : f2(a);
+        };
+    };
+
+    makeMonadWithAlternative(either, bind, alt, ret);
+
+})();
