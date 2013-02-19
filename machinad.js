f8a743be8897a584b5020bfe40b054dc783d8ca4
Your Name <you@example.com>
2013-02-18 23:21:56 -0700

    Nudged stuff here and there, simplified parsers added not, took parsers out of global

--- machinad.js
+++ machinad.js
@@ -30,60 +30,64 @@
 //    must follow 1f above in any derived works or modified forms of this.
 
 var makeMonadWithAlternative = function(m_, bind_, alt_, ret_) {
-    (function() {
-        /* monad instance */
-        var bind = function(f, m) {
-            return m !== undefined && m.m !== undefined && m.a !== undefined ? bind_(f, m) : m;
+    /* monad instance */
+    var bind = function(f, m) {
+        return m !== undefined && m.m !== undefined && m.a !== undefined ? bind_(f, m) : m;
+    };
+    var ret = ret_;
+
+    /* monad utility functions */
+    var kleisli = function(f1, f2) { return function(a) { return bind(f2, f1(a)); }; };
+
+    var liftM2 = function(f, m1, m2) {
+        return bind(function (fp) {
+            return bind(function(a1) {
+                return bind(function(a2) {
+                    return ret(fp(a1)(a2));
+                }, m2);
+            }, m1);
+        }, ret(f));
+    };
+
+    /* functor instance */
+    var map = function(m, f) { return bind(function(a) { return ret(f(a)); }, m); };
+
+    /* applicative instance; ret = return = pure */
+    var ap = function(mf, ma) {
+        return liftM2(function(a) { return a; }, mf, ma);
+    };
+
+    /* alternative instance */
+    var alt = alt_;
+
+    /* recursive kleisli fold until alternative is chosen */
+    var until = function(f, p) {
+        return function(a) {
+            return kleisli(f, alt(p, until(f, p)))(a);
         };
-        var ret = ret_;
-        var kleisli = function(f1, f2) { return function(a) { return bind(f2, f1(a)) }; };
+    };
 
-        var liftM2 = function(f, m1, m2) {
-            return bind(function (fp) {
-                return bind(function(a1) {
-                    return bind(function(a2) {
-                        return ret(fp(a1)(a2));
-                    }, m2);
-                }, m1);
-            }, ret(f));
-        };
+    var disregard = function(f1, f2) {
+        return f1.then(function(a) { bind(f2, ret(a)); return ret(a); });
+    };
 
-        /* functor instance */
-        var map = function(m, f) { return bind(function(a) { return ret(f(a)); }, m); };
+    (function(a, k, u, d) {
+        (function() {
+            this.or = function(f2) { return a(this, f2); };
+            this.then = function(f2) { return k(this, f2); };
+            this.until = function(p) { return u(this, p); };
+            this.disregard = function(f2) { return d(this, f2); };
+        }).call(Function.prototype);
+    })(alt, kleisli, until, disregard);
 
-        /* applicative instance; ret = return = pure */
-        var ap = function(mf, ma) {
-            return liftM2(function(a) { return a; }, mf, ma);
-        };
-
-        /* alternative instance */
-        var alt = alt_;
-
-        /* recursive kleisli fold until alternative is chosen */
-        var until = function(f, p) {
-            return function(a) {
-                return kleisli(f, alt(p, until(f, p)))(a); 
-            };
-        };
-
-        (function(a, k, u) {
-            (function() {
-                this.or = function(f2) { return a(this, f2); };
-                this.then = function(f2) { return k(this, f2); };
-                this.until = function(p) { return u(this, p); };
-            }).call(Function.prototype);
-        })(alt, kleisli, until);
-
-        (function(b, r, m, a) {
-            (function() {
-                this.bind = function(f) { return b(f, this); };
-                this.ret = function(a) { return r(a); };
-                this.map = function(f) { return m(this, f); };
-                this.ap = function(mf) { return a(mf, this); };
-            }).call(m_.prototype);
-        })(bind, ret, map, ap);
-
-    }).call(m_.prototype);
+    (function(b, r, m, a) {
+        (function() {
+            this.bind = function(f) { return b(f, this); };
+            this.ret = function(a) { return r(a); };
+            this.map = function(f) { return m(this, f); };
+            this.ap = function(mf) { return a(mf, this); };
+        }).call(m_.prototype);
+    })(bind, ret, map, ap);
 };
 
 // create the data type and monad instance
@@ -95,22 +99,32 @@ var makeMonadWithAlternative = function(m_, bind_, alt_, ret_) {
         catch(e) { this.a = $.extend(true, {}, a); }
     };
 
-    var Success = {}
-    var Failure = {}
+    var Success = {};
+    var Failure = {};
 
     Success.f = this.success = function(a) { return new either(Success, a); };
     Failure.f = this.failure = function(a) { return new either(Failure, a); };
 
-    var bind = function(f, m) { return m.m === Success ? f(m.a) : m; };
-    var ret = function(a) { return new success(a); };
+    (function() {
+        var bind = function(f, m) { return m.m === Success ? f(m.a) : m; };
+        var ret = function(a) { return new success(a); };
 
-    var alt = function(f1, f2) {
+        var alt = function(f1, f2) {
+            return function(a) {
+                var resultf1 = f1(a);
+                return resultf1.m === Success ? resultf1 : f2(a);
+            };
+        };
+
+        makeMonadWithAlternative(either, bind, alt, ret);
+
+    }).call(function(){});
+
+    /* not could be generic from alternative but it becomes shit perf */
+    this.not = function(f) {
         return function(a) {
-            var resultf1 = f1(a);
-            return resultf1.m === Success ? resultf1 : f2(a);
+            var result = f(a);
+            return result.m === Success ? failure(result.a) : success(result.a);
         };
     };
-
-    makeMonadWithAlternative(either, bind, alt, ret);
-
 })();
--- parsad.js
+++ parsad.js
@@ -29,64 +29,81 @@
 //    in your modified or derived version, because I'm not a dick. You still
 //    must follow 1f above in any derived works or modified forms of this.
 
-var untake = function(a) {
-    a.input.unshift(a.result.pop());
-    return success(a);
-};
+var p_ = {};
 
-var char = function(c) {
+/* parser primitives */
+p_.makeParseable = function(a) { return success({input: a.split(''), result: []}); };
+p_.skip = function(a){ a.input.shift(1); return success(a); };
+p_.take = function(a){ a.result.push(a.input.shift(1)); return success(a); };
+p_.untake = function(a) { a.input.unshift(a.result.pop()); return success(a); };
+p_.endOfParse = function(a) { return a.input.length < 1 ? success(a) : failure(a); };
+
+/* character primitives */
+p_.isChar = function(c) {
     return function(a) { return a.input[0] === c ? success(a) : failure(a); };
 };
 
-var getString = function(s) {
+p_.isCharCI = function(c) { return p_.isChar(c.toUpperCase()).or(p_.isChar(c.toLowerCase())); };
+
+p_.getChar = function(c) { return p_.isChar(c).then(p_.take); };
+
+p_.getCharCI = function(c) { return p_.isCharCI(c).then(p_.take); };
+
+p_.pushChar = function(c) {
+    return function(a) { a.result.push(c); return success(a); };
+};
+
+/* string primitives */
+p_.getString = function(s) {
     var s1 = s.split('');
     return (function(a) {s1 = JSON.parse(JSON.stringify(s)).split(''); return success(a); })
            .then(
-               (function(a){ return tryTake(s1[0])(a); })
-               .then(function(a) { s1.shift(); return success(a); })
+               (function(a){ return p_.getChar(s1[0]).disregard(function(){s1.shift();})(a); })
                .until(function(a) { return s1.length < 1 ? success(a) : failure(a); }))
-           .or(
-               (function(a) { return s1.length >= s.length ? failure(a) : success(a); })
-               .then(untake)
-               .then(function(a) { s1.unshift(a.input[0]); return success(a); })
-               .until(failure)
-           );
+           .or(failure);
 };
 
-var skipString = function(s) {
-    var i = s.length;
-    return getString(s)
-        .then(function(a) { i = s.length; return success(a); })
-        .then(untake.then(skip).until(function(a) { return --i === 0 ? success(a) : failure(a); }));
+
+p_.getStringCI = function(s) {
+    var s1 = s.split('');
+    return (function(a) {s1 = JSON.parse(JSON.stringify(s)).split(''); return success(a); })
+           .then(
+               (function(a){ return p_.getCharCI(s1[0]).disregard(function(){s1.shift();})(a); })
+               .until(function(a) { return s1.length < 1 ? success(a) : failure(a); }))
+           .or(failure);
 };
 
-var endOfParse = function(a) { return a.input.length < 1 ? success(a) : failure(a); };
-var skip = function(a){ a.input.shift(1); return success(a); };
-var take = function(a){ a.result.push(a.input.shift(1)); return success(a); };
-var tryTake = function(c) { return char(c).then(take).or(failure); };
-var pushChar = function(c) {
+p_.skipString = function(s) {
     return function(a) {
-        a.result.push(c);
-        return success(a);
+        return p_.getString(s).then(function(b) { a.input = b.input; return success(a); })(a);
     };
 };
 
-var pushString = function(s) {
+p_.skipStringCI = function(s) {
+    return function(a) {
+        return p_.getStringCI(s).then(function(b) { a.input = b.input; return success(a); })(a);
+    };
+};
+
+p_.pushString = function(s) {
     return function(a) {
         a.result = a.result.concat(s.split(''));
         return success(a);
     };
 };
 
-var makeParseable = function(a) { return success({input: a.split(''), result: []}); };
+p_.replaceString = function(oldS, newS) { return p_.skipString(oldS).then(p_.pushString(newS)); };
 
-var space = char(' ');
-var newLine = char('\n').or(char('\r'));
-var tab = char('\t');
-var whiteSpace = space.or(newLine).or(tab);
+p_.replaceStringCI = function(oldS, newS) { return p_.skipStringCI(oldS).then(p_.pushString(newS)); };
 
-var vowel = char('a')
-        .or(char('e'))
-        .or(char('i'))
-        .or(char('o'))
-        .or(char('u'));
+/* combinators */
+p_.space = p_.isChar(' ');
+p_.newLine = p_.isChar('\n').or(p_.isChar('\r'));
+p_.tab = p_.isChar('\t');
+p_.whiteSpace = p_.space.or(p_.newLine).or(p_.tab);
+
+p_.isVowel = p_.isChar('a')
+         .or(p_.isChar('e'))
+         .or(p_.isChar('i'))
+         .or(p_.isChar('o'))
+         .or(p_.isChar('u'));
