4d1c96496a70e5616d4c9f11ff0843103996061d
JimmyHoffa <Jimmy@hoffa.com>
2016-05-10 23:43:46 -0600

    I was mistaken in a variety of places regarding JS

--- machinad.js
+++ machinad.js
@@ -29,102 +29,94 @@
 //    in your modified or derived version, because I'm not a dick. You still
 //    must follow 1f above in any derived works or modified forms of this.
 
-var makeMonadWithAlternative = function(m_, bind_, alt_, ret_) {
-    /* monad instance */
-    var bind = function(f, m) {
-        return m !== undefined && m.m !== undefined && m.a !== undefined ? bind_(f, m) : m;
-    };
-    var ret = ret_;
-
-    /* monad utility functions */
-    var kleisli = function(f1, f2) { return function(a) { return bind(f2, f1(a)); }; };
-
-    var liftM2 = function(f, m1, m2) {
-        return bind(function (fp) {
-            return bind(function(a1) {
-                return bind(function(a2) {
-                    return ret(fp(a1)(a2));
-                }, m2);
-            }, m1);
-        }, ret(f));
-    };
-
-    /* functor instance */
-    var map = function(m, f) { return bind(function(a) { return ret(f(a)); }, m); };
-
-    /* applicative instance; ret = return = pure */
-    var ap = function(mf, ma) {
-        return liftM2(function(a) { return a; }, mf, ma);
-    };
-
-    /* alternative instance */
-    var alt = alt_;
-
-    /* recursive kleisli fold until alternative is chosen */
-    var until = function(f, p) {
-        return function(a) {
-            return kleisli(f, alt(p, until(f, p)))(a);
-        };
-    };
-
-    var disregard = function(f1, f2) {
-        return f1.then(function(a) { bind(f2, ret(a)); return ret(a); });
-    };
-
-    (function(a, k, u, d) {
-        (function() {
-            this.or = function(f2) { return a(this, f2); };
-            this.then = function(f2) { return k(this, f2); };
-            this.until = function(p) { return u(this, p); };
-            this.disregard = function(f2) { return d(this, f2); };
-        }).call(Function.prototype);
-    })(alt, kleisli, until, disregard);
-
-    (function(b, r, m, a) {
-        (function() {
-            this.bind = function(f) { return b(f, this); };
-            this.ret = function(a) { return r(a); };
-            this.map = function(f) { return m(this, f); };
-            this.ap = function(mf) { return a(mf, this); };
-        }).call(m_.prototype);
-    })(bind, ret, map, ap);
-};
-
-// create the data type and monad instance
 (function() {
+  // these 2 instances act as enums for reference equality to decide on our current state
+  var Success = {};
+  var Failure = {};
 
-    var either = function(m, a) {
-        this.m = m;
-        try { this.a = JSON.parse(JSON.stringify(a)); }
-        catch(e) { this.a = $.extend(true, {}, a); }
+  var eitherCtor = function(_m, _a) {
+    var res = {
+      m: _m,
+      a: _a
     };
 
-    var Success = {};
-    var Failure = {};
+    res.bind = function(f) { return window.either.bind(f, res); };
+    res.map = function(f) { return window.either.map(res, f); };
+    res.ap = function(mf) { return window.either.ap(mf, res); };
+    return res;
+  };
 
-    Success.f = this.success = function(a) { return new either(Success, a); };
-    Failure.f = this.failure = function(a) { return new either(Failure, a); };
+  /* alternative instance */
+  var alt = function(f1, f2) {
+    return function(a) {
+      var resultf1 = f1(a);
+      return resultf1 !== undefined && resultf1.m !== Failure ? resultf1 : f2(a);
+    };
+  };
 
+  /* monad instance */
+  var bind = function(f, m) { // I'm auto-returning non-monads into the monadic case with the assumption it's a `success`
+    return m !== undefined && m.m !== Failure ? f(m.m === Success ? m.a : m) : m; // innocent until proven guilty
+  };
+  var ret = function(a) { return eitherCtor(Success, a); };
+  var success = window.success = function(a) { return eitherCtor(Success, a); };
+  var failure = window.failure = function(a) { return eitherCtor(Failure, a); };
+
+  /* monad utility functions */
+  var kleisli = function(f1, f2) { return function(a) { return bind(f2, bind(f1, a)); }; };
+
+  var liftM2 = function(f, m1, m2) {
+    return bind(function (fp) {
+      return bind(function(a1) {
+        return bind(function(a2) {
+          return ret(fp(a1)(a2));
+        }, m2);
+      }, m1);
+    }, ret(f));
+  };
+
+  /* functor instance */
+  var map = function(m, f) { return bind(function(a) { return ret(f(a)); }, m); };
+
+  /* applicative instance; ret = return = pure */
+  var ap = function(mf, ma) {
+    return liftM2(function(a) { return a; }, mf, ma);
+  };
+
+  /* recursive kleisli fold until alternative is chosen */
+  var until = function(f, p) {
+    return function(a) {
+      return kleisli(f, alt(p, until(f, p)))(a);
+    };
+  };
+
+  var disregard = function(f1, f2) {
+    return function(a) {
+      return bind(function(b) { return bind(f2, ret(a));}, bind(f1, ret(a)));
+    };
+  };
+
+  (function(a, k, u, d) {
     (function() {
-        var bind = function(f, m) { return m.m === Success ? f(m.a) : m; };
-        var ret = function(a) { return new success(a); };
+      this.or = function(f2) { return a(this, f2); };
+      this.then = function(f2) { return k(this, f2); };
+      this.until = function(p) { return u(this, p); };
+      this.disregard = function(f2) { return d(this, f2); };
+    }).call(Function.prototype);
+  })(alt, kleisli, until, disregard);
 
-        var alt = function(f1, f2) {
-            return function(a) {
-                var resultf1 = f1(a);
-                return resultf1.m === Success ? resultf1 : f2(a);
-            };
-        };
+  window.either = {
+    ret: ret,
+    success: success,
+    failure: failure,
+    map: map,
+    bind: bind,
+    kleisli: kleisli,
+    liftM2: liftM2,
+    ap: ap,
+    alt: alt,
+    until: until,
+    disregard: disregard
+  };
 
-        makeMonadWithAlternative(either, bind, alt, ret);
-
-    }).call(function(){});
-
-    /* not could be generic from alternative but it becomes shit perf */
-    this.not = function(f) {
-        return function(a) {
-            var result = f(a);
-            return result.m === Success ? failure(result.a) : success(result.a);
-        };
-    };
-})();
+}).call({});
